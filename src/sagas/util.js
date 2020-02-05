8cc5345248f05904034d2d6a7a05ea43656984c6
ricardo <rleonr@protonmail.com>
2020-02-05 10:46:33 -0500

    Treat status 200 as success

--- src/sagas/util.js
+++ src/sagas/util.js
@@ -2,7 +2,7 @@ import { call, put } from 'redux-saga/effects';
 
 export function* apiCallSaga(action, service, types){
   const response = yield call(service, action.payload);
-  if(response.status > 200 && response.status < 300) {
+  if(response.status >= 200 && response.status < 300) {
     for (const action of types.success) {
       yield put({ type: action, payload: response.data });
     }
