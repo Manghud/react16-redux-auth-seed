5dbc6ce6398822b7d8a863908e67866cb1554449
ricardo <rleonr@protonmail.com>
2020-02-05 11:20:35 -0500

    Inject auth token into authenticated requests

--- src/services/services.js
+++ src/services/services.js
@@ -5,8 +5,10 @@ const DOMAIN_NAME = config.domainName;
 
 export const authServiceURL = `http://localhost:5151${DOMAIN_NAME}`;
 
-export const callAPI = config => axios({
-  ...config
-}).catch(error => {
-  return { ...error.response };
-});
+export const callAPI = (config) => {
+  const requestConfig = { ...config };
+  if (config.auth) {
+    requestConfig.headers['Authorization'] = localStorage.getItem('authToken');
+  }
+  return axios().catch(error => ({ ...error.response }));
+};
